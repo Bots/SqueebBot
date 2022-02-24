2c87b62edf2ba42c3e7401b6cdb6044bf3c834aa
John Paul Wile <botsone@gmail.com>
2022-02-23 20:54:30 -0700

    chore: added procfile edited deploy command script

--- deploy-commands.js
+++ deploy-commands.js
@@ -4,12 +4,19 @@ const { REST } = require('@discordjs/rest')
 const { Routes } = require('discord-api-types/v9')
 
 const commands = [
-	new SlashCommandBuilder().setName('get-random-follower').setDescription('Fetches a random follower from Glitchposts followers list')
-]
-	.map(command => command.toJSON())
-console.log(process.env.DISCORD_TOKEN)
+	new SlashCommandBuilder()
+		.setName('get-random-follower')
+		.setDescription('Fetches a random follower from Glitchposts followers list')
+].map(command => command.toJSON())
 const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN)
 
-rest.put(Routes.applicationGuildCommands(process.env.DISCORD_CLIENT_ID, process.env.DISCORD_GUILD_ID), { body: commands })
+rest
+	.put(
+		Routes.applicationGuildCommands(
+			process.env.DISCORD_CLIENT_ID,
+			process.env.DISCORD_GUILD_ID
+		),
+		{ body: commands }
+	)
 	.then(() => console.log('Successfully registered application commands.'))
 	.catch(console.error)
